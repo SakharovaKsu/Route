54270871944acb414db785dba016380e9b9d8c34
SakharovaKsu <nagis38@gmail.com>
2023-05-27 22:45:18 +0300

    Добавление библиотеки Router, вложенные компоненты
    Два варианта как можно сделать вложенные компоненты и небольшое пояснение по путям

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
 import './App.css';
+import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
+
+const Music = () => {
+    const params = useParams<'id'>()
+    const some = params
+    console.log(some)
+    return <div>Music</div>
+}
 
 function App() {
     return (
         <div className="App">
+            <NavLink to={'./'}> MAIN </NavLink>
+            <NavLink to={'./login'}> LOGIN </NavLink>
+            <NavLink to={'./profile'}> PROFILE </NavLink>
+
+            {/* вложенность*/}
+            <NavLink to={'./profile/setting'}> SETTING </NavLink>
+
+
+            <NavLink to={'./music'}> MUSIC </NavLink>
+            <NavLink to={'./music/1'}> MUSIC/1 </NavLink>
+
+            <Routes>
+                <Route path={'./*'} element={<div>404</div>} />
+                <Route path={'./'} element={<div>main</div>} />
+                <Route path={'./login'} element={<div>login</div>} />
+
+                {/* вложенность */}
+                {/* при клике на setting будет еще отображаться еще и profile */}
+                <Route path={'./profile/*'} element={(
+                    <div>
+                        profile
+                        <Routes>
+                            <Route path={'./setting'} element={<div>setting</div>} />
+                        </Routes>
+                    </div>
+                )} />
+
+                {/* вложенность 2 */}
+                {/* добавляем <Outlet /> на то место куда надо добавить */}
+                {/*<Route path={'./profile/*'} element={(<div>profile <Outlet /> </div>)} />*/}
+                {/*<Route path={'./setting'} element={<div>setting</div>} />*/}
+
+
+                <Route path={'./profile/:id'} element={<Music />} />
+                {/*  Тут после слеша говорим, что у нас будет какой-то id  */}
+            </Routes>
         </div>
     );
 }
 
 export default App;
+
+
+// -------
+// to это вместо href
+// NavLink используем, что б страница не перезагружалась
+
+// path={'./*'} path={'./login/*'} добавление звездочки означает, что после нее может быть все что угодно.
+// path={'./profile/:id'} - после двоеточия идет параметр.
+// все параметры являются строкой, если мы хотим число, то нужно ставить +, что б перевести в number
+// если нам надо два параметра path={'./profile/:id:x'}, в типизации уже указываем <id | x> (useParams<'id | x'>())
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
@@ -9,9 +10,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
+
+// 1. Оборачиваем App - BrowserRouter, для создания страниц
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
